94da02707cb41e0cc565fd5ff08144d9bca64715
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-05-17 13:49:47 -0400

    working on ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Product from './Product';
 import Title from './Title';
 import { storeProducts } from '../data';
+import { ProductConsumer } from '../context';
 
 export default class ProductList extends Component {
     state = {
@@ -14,7 +15,14 @@ export default class ProductList extends Component {
                 <div className="py-5">
                     <div className="container">
                         <Title name="our" title="products" />
-                        <div className="row" />
+                        
+                        <div className="row">
+                            <ProductConsumer>
+                                {(value)=>{
+                                    return <h1>{value}</h1>
+                                }}
+                            </ProductConsumer>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
