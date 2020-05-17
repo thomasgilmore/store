21fb95552138776f075e2ea13eb05e4b31db887b
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-05-17 14:54:52 -0400

    add title and price of products to footer of cards in Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,10 +9,35 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={console.log('you clicked me on the image container')}>
+                    <div className="img-container p-5" onClick={() => console.log('you clicked me on the image container')}>
                         <Link to="/details">
                             <img src={img} alt="product" className="card-img-top" />
                         </Link>
+                        <button 
+                            className="cart-btn" 
+                            disabled={ inCart ? true : false } 
+                            onClick={() => {
+                                console.log('added to the cart');
+                            }} 
+                        >
+                            {inCart ? (
+                                <p className="text-capitalize mb-0" disabled>
+                                {" "}
+                                in inCart
+                            </p>
+                            ) : (
+                            <i className="fas fa-cart-plus" />)}
+                        </button> 
+                    </div>
+                    {/* card footer */}
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
             </ProductWrapper>
