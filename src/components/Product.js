b83ed07956ac0d2758f96ba95b6a12b53b08dc96
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-05-18 12:37:42 -0400

    add Product.propTypes to Product.js to check data.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
     render() {
@@ -43,6 +44,16 @@ export default class Product extends Component {
             </ProductWrapper>
         )
     }
+}
+
+Product.propTypes = {
+    product: PropTypes.shape({
+        id: PropTypes.number,
+        img: PropTypes.string,
+        title: PropTypes.string,
+        price: PropTypes.number,
+        inCart: PropTypes.bool
+    }).isRequired
 }
 
 const ProductWrapper = styled.div`
