d023261f0fe2baa54a2eff697561a86b3d40dbc1
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-05-21 15:13:01 -0400

    working on cart layout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import Title from '../Title';
-
+import CartColumns from './CartColumns';
+import EmptyCart from './EmtpyCart';
+import { ProductConsumer } from '../../context';
 
 export default class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart" />
+                <ProductConsumer>
+                    {(value) => {
+                        const {cart} = value;
+                        if (cart.length > 0) {
+                            return (
+                                <React.Fragment>
+                                    <Title name="your" title="cart" />
+                                    <CartColumns />
+                                </React.Fragment>
+                            )
+                        } else {
+                            return <EmptyCart /> 
+                        }
+                    }}
+                </ProductConsumer>
             </section>
         )
     }
